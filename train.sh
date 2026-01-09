ef2d5b143364b220b290c350d49f9a83c5ff7317
FHTT <fhtt.tian@foxmail.com>
2026-01-09 15:37:32 +0800

    first commit

---
+++ train.sh
@@ -0,0 +1,22 @@
+# mosi
+#for i in $(seq 1111 1 1113)
+#do
+#  CUDA_VISIBLE_DEVICES=3 python -u train.py --config_file configs/train_mosi.yaml --seed $i >./log/mosi.txt 2>&1 &
+#  wait
+#done
+
+
+# mosei
+for i in $(seq 1113 1 1113)
+do
+  CUDA_VISIBLE_DEVICES=3 python -u train.py --config_file configs/train_mosei.yaml --seed $i >./log/mosei.txt 2>&1 &
+  wait
+done
+
+
+## sims
+#for i in $(seq 1111 1 1113)
+#do
+#  CUDA_VISIBLE_DEVICES=3 python -u train.py --config_file configs/train_sims.yaml --seed $i >./log/sims.txt 2>&1 &
+#  wait
+#done
