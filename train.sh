830fe57674711175648281302c5800e1c3132653
FHTT <fhtt.tian@foxmail.com>
2026-01-10 14:14:36 +0800

    new commit

--- train.sh
+++ train.sh
@@ -7,16 +7,19 @@
 
 
 # mosei
+#for i in $(seq 1113 1 1113)
+#do
+#  CUDA_VISIBLE_DEVICES=3 python -u train.py --config_file configs/train_mosei.yaml --seed $i >./log/mosei.txt 2>&1 &
+#  wait
+#done
+
+
+# sims
 for i in $(seq 1113 1 1113)
 do
-  CUDA_VISIBLE_DEVICES=3 python -u train.py --config_file configs/train_mosei.yaml --seed $i >./log/mosei.txt 2>&1 &
+  CUDA_VISIBLE_DEVICES=1 python -u train.py --config_file configs/train_sims.yaml --seed $i >./log/sims.txt 2>&1 &
   wait
 done
 
-
-## sims
-#for i in $(seq 1111 1 1113)
-#do
-#  CUDA_VISIBLE_DEVICES=3 python -u train.py --config_file configs/train_sims.yaml --seed $i >./log/sims.txt 2>&1 &
-#  wait
-#done
+# shell 输出日志运行命令 ->
+# CUDA_VISIBLE_DEVICES=1 python -u train.py --config_file configs/train_sims.yaml --seed 1111
