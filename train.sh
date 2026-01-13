fb83ac08f308631263b32b7920f68dd741339fbd
FHTT <fhtt.tian@foxmail.com>
2026-01-13 17:45:20 +0800

    new commit

--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
 # mosi
-#for i in $(seq 1111 1 1113)
-#do
-#  CUDA_VISIBLE_DEVICES=3 python -u train.py --config_file configs/train_mosi.yaml --seed $i >./log/mosi.txt 2>&1 &
-#  wait
-#done
+for i in $(seq 1111 1 1113)
+do
+  CUDA_VISIBLE_DEVICES=3 python -u train.py --config_file configs/train_mosi.yaml --seed $i >./log/mosi.txt 2>&1 &
+  wait
+done
 
 
 # mosei
-#for i in $(seq 1113 1 1113)
+#for i in $(seq 1111 1 1113)
 #do
 #  CUDA_VISIBLE_DEVICES=3 python -u train.py --config_file configs/train_mosei.yaml --seed $i >./log/mosei.txt 2>&1 &
 #  wait
@@ -15,11 +15,11 @@
 
 
 # sims
-for i in $(seq 1113 1 1113)
-do
-  CUDA_VISIBLE_DEVICES=1 python -u train.py --config_file configs/train_sims.yaml --seed $i >./log/sims.txt 2>&1 &
-  wait
-done
+#for i in $(seq 1111 1 1113)
+#do
+#  CUDA_VISIBLE_DEVICES=1 python -u train.py --config_file configs/train_sims.yaml --seed $i >./log/sims.txt 2>&1 &
+#  wait
+#done
 
 # shell 输出日志运行命令 ->
 # CUDA_VISIBLE_DEVICES=1 python -u train.py --config_file configs/train_sims.yaml --seed 1111
